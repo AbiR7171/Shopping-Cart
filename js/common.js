2c8314136d2ee7145d7fabc2c5c62dd662bbe96c
AbiR7171 <abiremtiag@gmail.com>
2023-03-18 16:21:43 +0600

    first commit

---
+++ js/common.js
@@ -0,0 +1,11 @@
+// document.getElementById('phone-Plus').addEventListener('click', ()=>{
+//     const phonePlus = document.getElementById('plus-number');
+//     const phonePlusValueString = phonePlus.value
+//     const phoneplusValue = parseInt(phonePlusValueString);
+// });
+
+function getValue(value){
+    const phoneNumber    = document.getElementById(value);
+    const phoneValueString =  phoneNumber.value;
+    const phoneValue = parseInt(phoneValueString);
+}
---
+++ js/shop.js
@@ -0,0 +1,68 @@
+
+
+document.getElementById('phone-Plus').addEventListener('click', ()=>{
+  const phonePlus = document.getElementById('plus-number');
+  const phonePlusValueString = phonePlus.value
+  const phoneplusValue = parseInt(phonePlusValueString);
+ 
+  const newPhonePlusValue = phoneplusValue + 1;
+  phonePlus.value = newPhonePlusValue;
+  
+  const phoneMOney =  document.getElementById('phone-money');
+//   const phoneMOneyString = phoneMOney.value;
+//   const phoneMoneyOrginal = parseFloat(phoneMOneyString);
+
+     phoneMOney.innerText = 1219 * newPhonePlusValue;
+
+});
+document.getElementById('phone-minus').addEventListener('click', ()=>{
+
+  const phoneMinus = document.getElementById('plus-number');
+  const phoneMinusValueString = phoneMinus.value
+  const phoneMinusValue = parseInt(phoneMinusValueString);
+ 
+  const newPhoneMinusValue = phoneMinusValue - 1;
+  phoneMinus.value = newPhoneMinusValue;
+  if(phoneMinus.value < 1){
+    phoneMinus.value = 1;
+    return
+  }
+  const phoneMOney =  document.getElementById('phone-money');
+//   const phoneMOneyString = phoneMOney.value;
+//   const phoneMoneyOrginal = parseFloat(phoneMOneyString);
+
+     phoneMOney.innerText = 1219 * newPhoneMinusValue;
+});
+document.getElementById('case-plus').addEventListener('click', ()=>{
+    const casePlus = document.getElementById('case-input');
+    const casePlusValueString = casePlus.value
+    const caseValue = parseInt(casePlusValueString);
+   
+    const newCaseValue = caseValue + 1;
+    casePlus.value = newCaseValue;
+    
+    const caseMOney =  document.getElementById('case-number');
+  //   const phoneMOneyString = phoneMOney.value;
+  //   const phoneMoneyOrginal = parseFloat(phoneMOneyString);
+  
+       caseMOney.innerText = 59 * newCaseValue;
+  
+  });
+  document.getElementById('case-minus').addEventListener('click', ()=>{
+
+    const caseMinus = document.getElementById('case-input');
+    const caseMinusValueString = caseMinus.value
+    const caseMinusValue = parseInt(caseMinusValueString);
+   
+    const newCaseMinusValue = caseMinusValue - 1;
+    caseMinus.value = newCaseMinusValue;
+    if(caseMinus.value < 1){
+      caseMinus.value = 1;
+      return
+    }
+    const caseMOney =  document.getElementById('case-number');
+  //   const phoneMOneyString = phoneMOney.value;
+  //   const phoneMoneyOrginal = parseFloat(phoneMOneyString);
+  
+       caseMOney.innerText = 59 * newCaseMinusValue;
+  });
